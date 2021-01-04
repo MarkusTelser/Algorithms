fb29e413c05a2a3711c38a64e57485d6a2bee66c
Markus Telser <markus.telser99@gmail.com>
2021-01-04 23:05:57 +0100

    added shell sort

---
+++ sorting_algorithm/shell_sort.py
@@ -0,0 +1,22 @@
+import time
+
+
+def sort(array):
+    start = time.time()
+    result = array.copy()
+    columns = [2147483647, 1131376761, 410151271, 157840433,
+               58548857, 21521774, 8810089, 3501671, 1355339, 543749, 213331,
+               84801, 27901, 11969, 4711, 1968, 815, 271, 111, 41, 13, 4, 1]
+    for k in range(0, len(columns)):
+        h = columns[k]
+        # sort with insertion sort
+        for i in range(h, len(result)):
+            t = result[i]
+            j = i
+            while j >= h and result[j - h] > t:
+                result[j] = result[j - h]
+                j = j - h
+            result[j] = t
+
+    end = time.time()
+    return result, end - start
