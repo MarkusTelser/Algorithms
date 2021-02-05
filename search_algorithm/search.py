6146aa701111cd139d46b30274e2c83a37f29133
Markus Telser <markus.telser99@gmail.com>
2021-02-05 18:38:46 +0100

    added other algo

--- search_algorithm/search.py
+++ search_algorithm/search.py
@@ -1,4 +1,3 @@
-from sorting_algorithm import counting_sort
 from search_algorithm import *
 from random import *
 
@@ -17,19 +16,11 @@ def run():
     size = 1000000
     array = random_data(size, 0, 1000)
     search = array[int(random() * size)]
-    names = ["Binary sort"]
-    times = [binary_search.sort(array, search)[1]]
+    names = ["Linear search", "Binary search"]
+    times = [linear_search.sort(array, search)[1], binary_search.sort(array, search)[1]]
     # sort and sync with names
     times, names = zip(*sorted(zip(times, names)))
     # print order of fastest sorting algorithms
     print("\nSort algorithms:")
     for i in range(len(times)):
         print(str(i + 1) + ".", names[i], "-", times[i])
-
-    # suggestion for algo to code
-    # A* search algorithm
-    # BM 25F
-    # depth first search
-    # breadth first search
-    # brepth first search
-    # deadth first search
